d46252ffa91dc0c622aef86de3a8732a2b7384d6
Remco de Leeuw <remcodeleeuw@live.nl>
2020-08-23 16:23:48 +0200

    Changed config for prod

--- src/config.js
+++ src/config.js
@@ -24,7 +24,18 @@ const dev = {
   }
 }
 
-const prod = {}
+const prod = {
+  cognito: {
+    REGION: 'eu-west-1',
+    USER_POOL_ID: 'eu-west-1_BuR4tucBV',
+    APP_CLIENT_ID: '93jah2t890u3d3k9m9dm6vced',
+    IDENTITY_POOL_ID: 'eu-west-1:6fbab3fa-4964-4312-a5ca-442ed7780267'
+  },
+  apiGateway: {
+    REGION: 'eu-west-1',
+    URL: 'https://api.mytravelwallet.nl/dev'
+  }
+}
 
 const config = () => {
   if (!process.env.REACT_APP_STAGE) {
